e7c96d8fcb140ab40c4b97e42513dd8eacaf8ba7
Michael Vernau <102547230+MichaelRuns@users.noreply.github.com>
2022-07-15 09:56:02 -0700

    added form submissions

--- api/main.py
+++ api/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from pydantic import BaseModel
 from typing import Optional
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -13,24 +14,42 @@ class LogisticRegression:
         self.theta = {}
 
     def fit(self, path):
+        print('training!')
         pass
 
     def predict(self, rev):
-        return "prediction from: " + rev
+        prediction = "[please train classifier]"
+        rev = str(rev)
+        print(rev)
+        if rev[0] == 'y':
+            prediction = "the first letter is y!"
+        else:
+            prediction = 'the first letter is not y!'
+        return "The result is:  " + prediction
 
 
 lr = LogisticRegression()
 templates = Jinja2Templates(directory="api/templates")
+lr.fit('asdf')
 
 
-@app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return "please go to ./form for the interactive code!"
 
 
-@app.get("/{review}")
-def predict(review: str):
-    return lr.predict(review)
+@app.post("/form")
+def predict(request: Request, test: str = Form(...)):
+    prediction = lr.predict(test)
+    return templates.TemplateResponse('index.html', context={'request': request, 'prediction': prediction})
+
+
+@app.get("/form")
+def predict(request: Request):
+    prediction = "Type a review"
+    return templates.TemplateResponse('index.html', context={'request': request, 'prediction': prediction})
+
+
 
 
 
